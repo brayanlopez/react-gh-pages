87c8e5148daf291db820a71fa02463640949a972
brayanlopez <balg.lg55@gmail.com>
2021-06-17 15:36:17 -0500

    Add colors and flexbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="container">
+      <h1>My first Github Pages!</h1>
+      <p>This is a test, we use this to learn how to use Github Pages.</p>
     </div>
   );
 }
